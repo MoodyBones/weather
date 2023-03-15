42cdcfa0538b5ad31ebf2c1369f20ca11150e3bb
Mel <jones.mel@gmail.com>
2023-03-15 17:31:46 +0100

    performance: add cache to event handler with swr stale-while-revalidate handler: * will store the response in a temporary memory cache * after the first request it reference the cache * and make the subsequent requests faster by returning the previous response

--- server/api/forecasts.get.ts
+++ server/api/forecasts.get.ts
@@ -1,14 +1,17 @@
-export default defineEventHandler(async event => {
-  const lat = 52.36
-  const lon = 4.9
+export default defineCachedEventHandler(
+  async event => {
+    const lat = 52.36
+    const lon = 4.9
 
-  // Nuxt helper $fetch https://nuxt.com/docs/api/utils/dollarfetch#fetch
-  // fetch external data
+    // Nuxt helper $fetch https://nuxt.com/docs/api/utils/dollarfetch#fetch
+    // fetch external data
 
-  const result = await $fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
-      useRuntimeConfig().openWeatherApiToken
-    }`
-  )
-  return result
-})
+    const result = await $fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+        useRuntimeConfig().openWeatherApiToken
+      }`
+    )
+    return result
+  },
+  { swr: true }
+)
