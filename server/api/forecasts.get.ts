edd40d95a56771b410b36be0b59d6be81755cd24
Mel <jones.mel@gmail.com>
2023-03-15 15:38:25 +0100

    fix: add linting

--- server/api/forecasts.get.ts
+++ server/api/forecasts.get.ts
@@ -1,12 +1,14 @@
 export default defineEventHandler(async event => {
+  const lat = 52.36
+  const lon = 4.9
 
-  let lat = 52.36
-  let lon = 4.90
-  
   // Nuxt helper $fetch https://nuxt.com/docs/api/utils/dollarfetch#fetch
-  // fetch external data=
+  // fetch external data
+
   const result = await $fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${useRuntimeConfig().openWeatherApiToken}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+      useRuntimeConfig().openWeatherApiToken
+    }`
   )
   return result
 })
