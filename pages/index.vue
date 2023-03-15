ffcd5012247d4c5139f3c91a45d9812d67849a81
Mel <jones.mel@gmail.com>
2023-03-15 16:49:42 +0100

    feat: handle forecast api fetch on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
+<script setup>
+const { data } = useFetch('/api/forecasts')
+</script>
+
 <template>
-  <article data-theme="dark">
-    <label for="city">Search</label>
-    <input type="text" name="city" placeholder="City" />
-  </article>
+  <div>
+    <article data-theme="dark">
+      <label for="city">Search</label>
+      <input type="text" name="city" placeholder="City" />
+    </article>
+    <article>
+      <h2>Here is the current data for {{ data.name }}</h2>
+      <ul>
+        <li>{{ data.main.temp }}</li>
+      </ul>
+    </article>
+  </div>
 </template>
