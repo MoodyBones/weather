971b183bbabb73186afcd825030fa006d26fd86d
Mel <jones.mel@gmail.com>
2023-03-15 15:48:00 +0100

    style: add picocss framework

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup>
+import '@picocss/pico'
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <h1>Weather</h1>
+    <label for="city">Search</label>
+    <input type="text" name="city" placeholder="City" />
   </div>
 </template>
