0911e38cfa93ba70e86b33534331a51ac7b5e6a3
Mel <jones.mel@gmail.com>
2023-03-21 17:47:28 +0100

    feat(wip): buggered up the request and blocked the api key

--- components/WeatherCurrentCard.vue
+++ components/WeatherCurrentCard.vue
@@ -2,26 +2,43 @@
 import { useCityStore } from '@/stores/cityStore'
 
 const cityStore = useCityStore()
+const current = ref(null)
 
-// destructure & make them reactive
-
-if (cityStore.isCityValid) {
-  const {
-    data: current,
-    error,
-    pending,
-  } = await useFetch('/api/current', {
-    query: { lat: cityStore.lat, lon: cityStore.lon },
+async function fetchCurrentWeather() {
+  const { pending, data, error } = useLazyFetch('/api/current', {
+    query: {
+      lat: cityStore.city.geolocation.lat,
+      lon: cityStore.city.geolocation.lon,
+    },
     pick: ['name', 'main', 'wind', 'sys'],
   })
-  current.value = data
+
+  current.value = await data.value
   if (error) console.log('error', error)
   if (pending) console.log('pending', pending)
 }
+
+onServerPrefetch(async () => {
+  // component is rendered as part of the initial request
+  // pre-fetch data on server as it is faster than on the client
+  current.value = await fetchCurrentWeather()
+})
+
+onMounted(async () => {
+  if (!current.value) {
+    // if current is null on mount, it means the component
+    // is dynamically rendered on the client. Perform a
+    // client-side fetch instead.
+    current.value = await fetchCurrentWeather()
+  }
+})
 </script>
 
 <template>
   <article>
+    <div v-if="pending?.length">
+      {{ error }}
+    </div>
     <div v-if="error?.length">
       {{ error }}
     </div>
