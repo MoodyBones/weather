501d8f2a5672ed57e6e46e31a380ae0e58b67c18
Mel <jones.mel@gmail.com>
2023-03-21 15:27:24 +0100

    feat: switch to typescript and add types

---
+++ stores/cityStore.ts
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia'
+
+interface CityInfo {
+  name: string
+  country: string
+  geolocation: {
+    lat: number
+    lon: number
+  }
+}
+
+export const useCityStore = defineStore('cityStore', {
+  state: () => ({
+    city: null as CityInfo | null,
+    cities: [] as CityInfo[],
+    hasCityChanged: false as boolean,
+  }),
+
+  // actions are methods
+  // mutate stores state with actions
+  actions: {
+    setCity(city: CityInfo) {
+      this.city = city
+    },
+    previousCities(city: CityInfo) {
+      return this.cities.push(city)
+    },
+  },
+
+  // getters are computed properties
+})
