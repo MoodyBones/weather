fb5812b62d0a85e8104e8295fcd43e71f75ba4f1
Mel <jones.mel@gmail.com>
2023-03-21 16:25:10 +0100

    fix: handle query parameters for current weather fetch * delete old test query

--- server/api/current.get.js
+++ server/api/current.get.js
@@ -1,15 +1,12 @@
 export default defineEventHandler(async event => {
-  console.log('event', event.context.query)
-  // const lat = event.context.query.lat
-  // const lon = event.context.query.lon
-
-  const lat = 52.36
-  const lon = 4.9
+  const query = getQuery(event)
+  // server util docs
+  // https://nuxt.com/docs/guide/directory-structure/server#handling-requests-with-query-parameters
 
   const result = await $fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
-      useRuntimeConfig().openWeatherApiToken
-    }`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${query.lat}&lon=${
+      query.lon
+    }&appid=${useRuntimeConfig().openWeatherApiToken}`
   )
   return result
 })
